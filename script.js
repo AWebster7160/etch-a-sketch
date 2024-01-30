9cea8d0cd22c1bd721be34167135bbb9575e91bc
AWebster7160 <156373395+AWebster7160@users.noreply.github.com>
2024-01-30 14:30:35 -0500

    Finish etch a sketch with random RGB

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
 let cube = document.createElement('div');
 const grid = document.querySelector('.grid');
+const reset = document.createElement('button');
+const body = document.querySelector('body');
+reset.setAttribute('id', 'reset');
+body.appendChild(reset);
+reset.innerHTML = ('Reset the grid');
+reset.onclick = (() => {
+    setGrid();
+})
 
-for (i=0; i<256; i++) {
+function getRandomRGB() {
+    red = Math.floor(Math.random()*255+1);
+    green = Math.floor(Math.random()*255+1);
+    blue = Math.floor(Math.random()*255+1);
+}
+
+function setGrid() {
+getGrid();
+
+while (grid.firstChild){
+    grid.removeChild(grid.firstChild);
+} 
+
+let cubeSize = ((640 - gridSize * 2)/gridSize);
+for (i=0; i<(gridSize**2); i++) {
     cube = document.createElement('div');
     cube.setAttribute('id', i);
+    cube.style.width = cubeSize + 'px';
+    cube.style.height = cubeSize + 'px';
     grid.appendChild(cube);
+    }
 }
 
-function etch(e) {
-    if (e.target === grid){
-        return;
+function getGrid() {
+    gridSize = prompt('Enter grid size');
+    if (gridSize > 100) {
+        alert('Too big!');
+        getGrid();
     }
-    e.target.style.backgroundColor='black';
+    else {
+        return gridSize;
+    }
 }
+
+function getRandomRGB() {
+        
+}
+
+setGrid();
+function etch(e) {
+    if (e.target === grid) {return};
+    red = Math.floor(Math.random()*255+1);
+    green = Math.floor(Math.random()*255+1);
+    blue = Math.floor(Math.random()*255+1);
+    e.target.style.backgroundColor = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+    
+}
+
 grid.addEventListener('mouseover', etch);
+
+
 
